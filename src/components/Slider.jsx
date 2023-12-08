c1092790427ff1b926133a737667686a609b72af
dougladmo <Douglas.costa7417@gmail.com>
2023-12-08 13:49:45 -0300

    ul do slider finalizazda

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,24 +1,30 @@
 import React, { useState } from 'react';
-import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
+import { MdOutlineKeyboardArrowLeft, MdOutlineKeyboardArrowRight } from 'react-icons/md';
 import { RxDotFilled } from 'react-icons/rx';
+import background1 from '../assets/background1.jpg'
+import background2 from '../assets/background2.jpg'
+import background3 from '../assets/background3.jpg'
 
 function Slider() {
   const slides = [
     {
       title: 'Desenvolvimento Java',
+      titleColor: '#3accc3',
       li: ['Desenvolvimento Java 6 a 21 LTS', 'Spring Framework and Spring Boot', 'Microsservice com SpringBoot', 'Integrações com API REST Full', 'Front end JFS, JSP e Angular (Node.js)', 'APIGEE Edge Gerenciamento de API'],
-      url: 'https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2620&q=80',
+      url: background1,
     },
     {
       title: 'Loja Virtual Crie a sua',
+      titleColor: '#f2bf5e',
       liTitle: 'Planos a partir de R$ 29/mês. Teste 15 dias grátis.',
       li: ['Publique seus produtos com fotos e vídeos', 'Atraia novos clientes, gere leads', 'O cliente acompanha todo o pedido', 'Produtos em Destaques e Ofertas.'],
-      url: 'https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
+      url: background2,
     },
     {
       title: 'ADVPL Protheus',
+      titleColor: '#3accc3',
       li: ['Flexibilidade no ERP', 'Personalização de Processos', 'Desenvolvimento de Rotinas, Pontos de entradas', 'O Sistema cresce junto com a empresa.'],
-      url: 'https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80',
+      url: background3,
     },
   ];
 
@@ -44,20 +50,24 @@ function Slider() {
     <div className='h-[600px] bg-blue-900 w-full m-auto relative group'>
       <div
         style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
-        className='md:w-11/12 m-auto h-full bg-center bg-cover duration-500'
+        className='md:w-11/12 m-auto h-full flex flex-col items-center justify-between py-10 px-5 bg-center bg-cover duration-500'
       >
-        <h1 className='text-center text-4xl pt-8 text-white uppercase'>{slides[currentIndex].title}</h1>
-        <ul>
-            <h2>{slides[currentIndex].liTitle? `${slides[currentIndex].liTitle}` : ''}</h2>
+        <h1 style={{color: `${slides[currentIndex].titleColor}`}} className={`text-left leading-normal text-4xl pt-8 font-bold uppercase`}>{slides[currentIndex].title}</h1>
+            <button className='uppercase ' >Saiba mais</button>
+        <ul className='list-disc'>
+            <h2 className='text-2xl max-w-xs text-left text-white' >{slides[currentIndex].liTitle? `${slides[currentIndex].liTitle}` : ''}</h2>
+            {slides[currentIndex].li?.map((item) => {
+              return <li className='text-lg ml-8 text-white'>{item}</li>
+            })}
         </ul>
       </div>
       {/* Left Arrow */}
-      <div className='absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl p-2 bg-gray-300 hover:opacity-50 transition duration-300 text-black cursor-pointer'>
-        <BsChevronCompactLeft onClick={prevSlide} size={40} />
+      <div className='absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-sm p-2 bg-gray-300 hover:opacity-50 transition duration-300 text-black cursor-pointer'>
+        <MdOutlineKeyboardArrowLeft onClick={prevSlide} size={40} />
       </div>
       {/* Right Arrow */}
-      <div className='absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl p-2 bg-gray-300 hover:opacity-50 transition duration-300 text-black cursor-pointer'>
-        <BsChevronCompactRight onClick={nextSlide} size={40} />
+      <div className='absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-sm p-2 bg-gray-300 hover:opacity-50 transition duration-300 text-black cursor-pointer'>
+        <MdOutlineKeyboardArrowRight onClick={nextSlide} size={40} />
       </div>
       <div className='flex top-4 justify-center py-2'>
         {slides.map((slide, slideIndex) => (
@@ -66,7 +76,7 @@ function Slider() {
             onClick={() => goToSlide(slideIndex)}
             className='text-2xl cursor-pointer'
           >
-            <RxDotFilled />
+            <RxDotFilled size={35} key={slideIndex} className={`transition duration-300 ${slideIndex == currentIndex ? 'text-blue-900'  : 'text-black'}`} />
           </div>
         ))}
       </div>
