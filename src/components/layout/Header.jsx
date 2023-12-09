a4fa4de978b35ff28cea8aa9450d3a848c09da98
dougladmo <Douglas.costa7417@gmail.com>
2023-12-09 16:32:33 -0300

    bug header corrigido

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -12,11 +12,8 @@ const Header = () => {
       const HandleScroll = () => {
         const scrollTop = window.scrollY;
         console.log(scrollTop)
-        if (scrollTop > 100) {
+        if (scrollTop > 130) {
           setScrolled(true);
-          if (scrollTop > 600) {
-            setScrolled(false)
-          }
         } else {
           setScrolled(false);
         }
@@ -30,12 +27,12 @@ const Header = () => {
     }, []);
 
   return (
-    <div className={`${ scrolled ? styles.sticky : '' }`} >
+    <div className={`${ scrolled ? styles.sticky : styles.header }`} >
       <div className='w-full sticky h-24 flex items-center'>
-          <nav id='header'  className='relative w-full px-3 flex justify-between md:justify-center md:items-center md:gap-14'>
+          <nav id='header'  className='relative w-full px-3 z-30 flex justify-between md:justify-center md:items-center md:gap-14'>
               <img className='w-48 self-center' src={Logo} alt="Casta logo" />
               <ul
-              className={`${offCanvas ? 'fixed' : 'hidden'} z-10 pt-28 md:pt-0 px-5 bg-blue-950 md:bg-transparent left-0 h-screen md:h-24 md:flex md:justify-center md:items-center md:gap-24 w-3/5 md:w-auto text-gray-300 md:text-blue-900 font-semibold text-xl md:text-base`}
+              className={`${offCanvas ? 'absolute' : 'hidden'} z-30 pt-28 md:pt-0 px-5 bg-blue-950 md:bg-transparent left-0 h-screen md:h-24 md:flex md:justify-center md:items-center md:gap-24 w-3/5 md:w-auto text-gray-300 md:text-blue-900 font-semibold text-xl md:text-base`}
               >
                   <li className='border-b-2 mb-3 pb-3 md:pb-0 md:mb-0 border-gray-300 md:border-none'><Link className='hover:text-blue-500 transition duration-500' to='/'>Home</Link></li>
                   <li className='border-b-2 mb-3 pb-3 md:pb-0 md:mb-0 border-gray-300 md:border-none'><Link className='hover:text-blue-500 transition duration-500' to='solucoes' >+  Soluções</Link></li>
