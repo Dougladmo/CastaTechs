c2c72e7c247d1bc88fe31edad0aad23167d4c6b5
dougladmo <Douglas.costa7417@gmail.com>
2023-12-29 12:42:19 -0300

    correções acessibilidade

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -39,10 +39,10 @@ const Header = () => {
               className={`${offCanvas ? 'absolute animate-offCanvas' : 'hidden'}  z-30 pt-12 lg:pt-0 px-5 bg-blue-950 lg:bg-transparent left-0 h-screen lg:h-24 lg:flex lg:justify-center lg:items-center lg:gap-24 w-3/5 lg:w-auto text-gray-300 lg:text-blue-900 font-semibold text-xl lg:text-base`}
               >
                   <li><Link to='/'  aria-label='link to homepage'><img className='my-10 rounded-lg w-44 md:hidden' src={Logo} alt="Casta logo  ul"/></Link></li>
-                  <li className='pb-3 mb-3 border-b-2 border-gray-300 lg:pb-0 lg:mb-0 lg:border-none'><Link className='transition duration-500 hover:text-white md:hover:text-blue-500' to='/'  aria-label='link to homepage'>Home</Link></li>
-                  <li className='pb-3 mb-3 border-b-2 border-gray-300 lg:pb-0 lg:mb-0 lg:border-none'><Link className='transition duration-500 hover:text-white md:hover:text-blue-500' to='solucoes'  aria-label='link to solutions page' >+  Soluções</Link></li>
-                  <li className='pb-3 mb-3 border-b-2 border-gray-300 lg:pb-0 lg:mb-0 lg:border-none'><Link className='transition duration-500 hover:text-white md:hover:text-blue-500' to='servicos'  aria-label='link services page' >Serviços</Link></li>
-                  <li className='pb-3 mb-3 border-b-2 border-gray-300 lg:pb-0 lg:mb-0 lg:border-none'><Link className='transition duration-500 hover:text-white md:hover:text-blue-500' to='sobre'  aria-label='link to about page' >A Casta</Link></li>
+                  <li className='pb-3 mb-3 border-b-2 border-gray-300 lg:pb-0 lg:mb-0 lg:border-none'><Link className='transition duration-500 hover:text-white md:hover:text-blue-500' to='/'  aria-label='Home'>Home</Link></li>
+                  <li className='pb-3 mb-3 border-b-2 border-gray-300 lg:pb-0 lg:mb-0 lg:border-none'><Link className='transition duration-500 hover:text-white md:hover:text-blue-500' to='solucoes'  aria-label='+ Soluções' >+  Soluções</Link></li>
+                  <li className='pb-3 mb-3 border-b-2 border-gray-300 lg:pb-0 lg:mb-0 lg:border-none'><Link className='transition duration-500 hover:text-white md:hover:text-blue-500' to='servicos'  aria-label='Serviços' >Serviços</Link></li>
+                  <li className='pb-3 mb-3 border-b-2 border-gray-300 lg:pb-0 lg:mb-0 lg:border-none'><Link className='transition duration-500 hover:text-white md:hover:text-blue-500' to='sobre'  aria-label='A Casta' >A Casta</Link></li>
                   <li className=''><Link className='transition duration-500 hover:text-white md:hover:text-blue-500'  to='contato' aria-label='link to contact page'>Contato</Link ></li>
               </ul>
               <div onClick={() => offCanvas ? setOffCanvas(false) : setOffCanvas(true)} className='flex items-center self-center justify-center p-3 mr-4 text-white rounded-lg bg-blue-950 h-14 w-14 lg:hidden'>
