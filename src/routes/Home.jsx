f24b6890b30dd3abb326e3ef719bb9a982a692fd
dougladmo <Douglas.costa7417@gmail.com>
2024-01-03 15:14:46 -0300

    casta tema black implementado

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -26,19 +26,22 @@ const Home = () => {
   return (
     <div id='main' className='w-full bg-black'>
       <Slider />
-      <div id='solucoes' className='w-full py-5 m-auto bg-gray-100 md:w-9/12 mt-14'>
-        <h2 className='pb-10 m-auto text-4xl font-bold text-center text-blue-500 drop-shadow-md md:text-5xl md:mx-28'>
-            <AttentionSeeker className='flex flex-col gap-6' effect='pulse' cascade damping={0.3}>
-              <span className='uppercase md:self-start'>Soluções que</span><span className='uppercase md:self-center'>transformam</span><span className='uppercase md:self-end'>seu negócio</span>
-            </AttentionSeeker>
-          </h2>
+      <div id='solucoes' className='w-full py-5 md:w-9/12 m-auto mt-14'>
+        <h3 className='pb-10 m-auto text-4xl font-bold text-center text-blue-500 drop-shadow-md md:text-5xl md:mx-28'>
+          <AttentionSeeker className='flex flex-col gap-6' effect='pulse' cascade damping={0.3}>
+            <span className='uppercase md:self-start'>Soluções que</span><span className='uppercase md:self-center'>transformam</span><span className='uppercase md:self-end'>seu negócio</span>
+          </AttentionSeeker>
+        </h3>
         <Fade duration={1500}>
           <div id='sua-loja-wrapper' className='lg:items-baseline lg:grid lg:grid-cols-2 lg:justify-evenly py-10'>
             <Zoom triggerOnce duration={1000}>
-              <div id='sua-loja' className='md:h-full'>
-                <img className='w-40 m-auto md:w-44' src={Cart} alt="litle cart store" />
-                <h3 className='max-w-[340px] m-auto text-center text-slate-700 font-semibold drop-shadow-md text-3xl'>CRIE SUA LOJA VIRTUAL</h3>
-                <div className='shadow-top flex flex-col lg:gap-5 text-slate-700 font-semibold items-center max-w-[340px] m-auto mt-4'>
+              <div id='sua-loja' style={{
+                backgroundImage: `linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)),
+                url(${gifBackground})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover'
+              }} className='md:h-full md:w-9/12 md:rounded-xl md:py-10 md:m-auto shadow-inner py-10'>
+                <img className='w-40 m-auto md:w-32 bg-white rounded-xl mb-5' src={Cart} alt="litle cart store" />
+                <h4 className='max-w-[340px] m-auto text-center text-white font-semibold drop-shadow-md text-3xl'>CRIE SUA LOJA VIRTUAL</h4>
+                <div className='shadow-top flex flex-col lg:gap-5 text-white font-semibold items-center max-w-[340px] m-auto mt-4'>
                   <h4 className='my-2 text-2xl text-center drop-shadow-lg'>E-COMMERCE</h4>
                   <ul className='flex flex-col max-w-xs m-auto ml-5 text-lg font-normal text-left list-disc lg:h-72 drop-shadow-md'>
                     <li>Sua Loja Aberta 24hrs por dia</li>
@@ -56,10 +59,14 @@ const Home = () => {
               </div>
             </Zoom>
             <Zoom triggerOnce duration={1000}>
-              <div id='totvs consultoria' className='md:h-full md:mt-0 mt-7'>
-                <img className='w-40 m-auto md:w-60' src={Consul} alt="litle cart store" />
-                <h4 className='max-w-[340px] md:max-w-none m-auto text-center text-slate-700 font-semibold drop-shadow-md text-3xl'>CONSULTORIA ERP TOTVS</h4>
-                <div className='shadow-top flex flex-col lg:gap-[19.5px] text-slate-700 font-semibold items-center max-w-[340px] m-auto mt-4'>
+              <div id='totvs-consultoria'
+              style={{
+                backgroundImage: `linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url(${gifBackground})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover'}} 
+              className='md:h-full md:w-9/12 md:rounded-xl md:py-10 md:m-auto shadow-inner py-10 mt-5'
+              >
+                <img className='w-40 m-auto md:w-60 bg-white rounded-xl mb-5' src={Consul} alt="litle cart store" />
+                <h4 className='max-w-[340px] md:max-w-none m-auto text-center text-white font-semibold drop-shadow-md text-3xl'>CONSULTORIA ERP TOTVS</h4>
+                <div className='shadow-top flex flex-col lg:gap-[19.5px] text-white font-semibold items-center max-w-[340px] m-auto mt-4'>
                   <h4 className='my-2 text-2xl text-center drop-shadow-lg'>PROTHEUS e RM</h4>
                   <ul className='flex flex-col max-w-xs m-auto ml-5 text-lg font-normal text-left list-disc lg:h-72 drop-shadow-md'>
                     <li>Implantação de sistemas ERP TOTVS</li>
@@ -135,37 +142,37 @@ const Home = () => {
           </Bounce>
         </div>
       </div>
-      <div id='fale-conosco' className='flex flex-col w-full py-10 bg-cyan-500'>
-          <AttentionSeeker effect='pulse' duration={1000} >
-            <div className='flex flex-col items-center justify-center w-full px-5 py-12 pt-0 text-center text-white bg-cyan-500 gap-7'>
-              <h3 className='text-5xl font-normal tracking-widest uppercase drop-shadow-lg'>Fale <span className='block pt-5'>conosco</span></h3>
-              <p className='text-2xl font-medium drop-shadow-lg lg:text-xl lg:font-normal font'>Temos satisfação em atender!
-                Queremos ouvir sobre sua necessidade e propor a melhor solução.</p>
-            </div>
-          </AttentionSeeker>
-            <div id='contact-cards' className='lg:flex lg:m-auto lg:w-3/4'>
-              <div className={`h-72 px-5 w-full flex flex-col text-white items-center justify-center gap-2 bg-[#a6c54a]`}>
-                <AttentionSeeker triggerOnce effect='shakeX' duration={1000} >
-                  <IoIosPhonePortrait size={55} className='text-white' />
-                  <h4 className='text-4xl font-medium drop-shadow-md'>Fones</h4>
-                  <p className='mb-2 text-xl text-center drop-shadow-lg'>(11) 96061-5651 <span className='block pt-2'>(11) 96061-5651</span></p>
-                </AttentionSeeker>
-              </div>
-              <div className={`h-72 px-5 w-full flex flex-col items-center justify-center gap-4 bg-[#f6f6f6]`}>
-                <AttentionSeeker triggerOnce effect='shakeX' duration={1000} >
-                  <FaRegPaperPlane size={55} />
-                  <h4 className='text-3xl font-medium drop-shadow-md'>Email</h4>
-                  <p className='mb-2 text-xl text-center drop-shadow-lg'>contato@castatecnologia.com <span className='block pt-2'>info@castatecnologia.com</span></p>
-                  </AttentionSeeker>
-              </div>
-              <div className={`h-72 px-5 w-full flex flex-col text-white items-center justify-center gap-5 bg-[#437da3]`}>
-                <AttentionSeeker triggerOnce effect='shakeX' duration={1000} >
-                  <IoLogoSkype size={55} />
-                  <h4 className='text-3xl font-medium drop-shadow-md'>Skype</h4>
-                  <p className='mb-2 text-xl text-center drop-shadow-lg'>Casta Tecnologia</p>
-                </AttentionSeeker>
-              </div>
-            </div>
+      <div id='fale-conosco' className='flex flex-col w-full py-10 bg-[#01b1af]'>
+        <AttentionSeeker effect='pulse' duration={1000} >
+          <div className='flex flex-col items-center justify-center w-full px-5 py-12 pt-0 text-center bg-[#01b1af] text-white bg-transparent gap-7'>
+            <h3 className='text-5xl font-normal tracking-widest uppercase drop-shadow-lg'>Fale <span className='block pt-5'>conosco</span></h3>
+            <p className='text-2xl font-medium drop-shadow-lg lg:text-xl lg:font-normal font'>Temos satisfação em atender!
+              Queremos ouvir sobre sua necessidade e propor a melhor solução.</p>
+          </div>
+        </AttentionSeeker>
+        <div id='contact-cards' className='lg:flex lg:m-auto lg:w-3/4'>
+          <div className={`h-72 px-5 w-full flex flex-col text-white items-center justify-center gap-2 bg-[#a6c54a]`}>
+            <AttentionSeeker triggerOnce effect='shakeX' duration={1000} >
+              <IoIosPhonePortrait size={55} className='text-white' />
+            </AttentionSeeker>
+            <h4 className='text-4xl font-medium drop-shadow-md'>Fones</h4>
+            <p className='mb-2 text-xl text-center drop-shadow-lg'>(11) 96061-5651 <span className='block pt-2'>(11) 96061-5651</span></p>
+          </div>
+          <div className={`h-72 px-5 w-full flex flex-col items-center justify-center gap-4 bg-[#f6f6f6]`}>
+            <AttentionSeeker triggerOnce effect='shakeX' duration={1000} >
+              <FaRegPaperPlane size={55} />
+            </AttentionSeeker>
+            <h4 className='text-3xl font-medium drop-shadow-md'>Email</h4>
+            <p className='mb-2 text-xl text-center drop-shadow-lg'>contato@castatecnologia.com <span className='block pt-2'>info@castatecnologia.com</span></p>
+          </div>
+          <div className={`h-72 px-5 w-full flex flex-col text-white items-center justify-center gap-5 bg-[#437da3]`}>
+            <AttentionSeeker triggerOnce effect='shakeX' duration={1000} >
+              <IoLogoSkype size={55} />
+            </AttentionSeeker>
+            <h4 className='text-3xl font-medium drop-shadow-md'>Skype</h4>
+            <p className='mb-2 text-xl text-center drop-shadow-lg'>Casta Tecnologia</p>
+          </div>
+        </div>
       </div>
     </div>
   )
